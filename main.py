ed4635dbbaaea183c019858914f44065b3684929
ELAND\weichiehhsu <weichiehhsu@eland.com.tw>
2024-06-12 15:19:26 +0800

    python 自學 範例程式

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-#%%
-import getpass
-import os
-import google.generativeai as genai
+# %%
+# import getpass
+# import os
+# import google.generativeai as genai
+# import test
+# import googleChatTest
+# from openai import AzureOpenAI
 import test
-import googleChatTest
-from openai import AzureOpenAI
+from test import Point
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
+    list1 = [1, 3, 5, 7]
+    print(list1[::1])
+    a: bool = True
+    result = (1, 2)[a]
+    print(result)
+    p1 = Point(0, 0)
+    p2 = Point(0, 0)
+    p3 = Point(x=0, y=5)
+    p4 = Point(y=5, x=0)
+    test.where_is(p3)
+    print(p1)
+    print(p2)
+    print(p3)
+    print(p4)
 
 # content = input("Enter you Question:")
 # response = test.generate_content(content)
--- test.py
+++ test.py
@@ -1,17 +1,35 @@
-import os
-import google.generativeai as genai
-from env_settings import EnvSettings
-import os
+# import os
+# import google.generativeai as genai
+# from env_settings import EnvSettings
+# import os
 
 
+#
+# def generate_content(content):
+#     prompt = ("1.請使用中文回答"
+#               "2.請舉範例\n")
+#     env_settings = EnvSettings()
+#     genai.configure(api_key=env_settings.GOOGLE_API_KEY, transport='rest')
+#     model = genai.GenerativeModel('gemini-pro')
+#     response = model.generate_content(prompt + content)
+#     # print(response.text)
+#     return response.text
+#
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-def generate_content(content):
-    prompt = ("1.請使用中文回答"
-              "2.請舉範例\n")
-    env_settings = EnvSettings()
-    genai.configure(api_key=env_settings.GOOGLE_API_KEY, transport='rest')
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(prompt + content)
-    # print(response.text)
-    return response.text
 
+def where_is(point):
+    match point:
+        case Point(x=0, y=0):
+            print("Origin")
+        case Point(x=0, y=y):
+            print(f"Y={y}")
+        case Point(x=x, y=0):
+            print(f"X={x}")
+        case Point():
+            print("Somewhere else")
+        case _:
+            print("Not a point")
